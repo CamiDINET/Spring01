1fca18a66dc4ec1a182a98b5bb516c3982fa22ac
CamiDINET <camidinet@gmail.com>
2023-03-13 15:16:47 +0100

    first quest spring

---
+++ src/main/java/com/wildcodeschool/spring01Demarrage/Spring01DemarrageApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.spring01Demarrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring01DemarrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring01DemarrageApplication.class, args);
+	}
+
+}
